597b276b25750da0ca4aa1d6ad53d7c3aabfd1a9
Lautaro Duarte <lautaroaugustoduarte@gmail.com>
2026-04-28 00:46:29 -0300

    remove CreateEvent (no requerido)

--- TPTicketingPS.API/obj/Debug/net8.0/TPTicketingPS.API.AssemblyInfo.cs
+++ TPTicketingPS.API/obj/Debug/net8.0/TPTicketingPS.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPTicketingPS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46fe3f137b078611de83a637ca84ba2041a486e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7245a82d7cc8e561f181463e8adc434ea787f3f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPTicketingPS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPTicketingPS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
