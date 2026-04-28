cd99287d0cb695fc9597f1a71b51a2e5fd675ac9
Lautaro Duarte <lautaroaugustoduarte@gmail.com>
2026-04-28 18:50:00 -0300

    events UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,17 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [events, setEvents] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetch("https://localhost:39716/api/V1/events")
+    fetch(`https://localhost:39716/api/V1/events?page=${page}&pageSize=10`)
       .then(res => res.json())
       .then(data => {
         console.log("DATA:", data);
         setEvents(data);
       })
       .catch(err => console.error(err));
-  }, []);
+  }, [page]);
 
   return (
     <div>
@@ -24,6 +25,20 @@ function App() {
           </li>
         ))}
       </ul>
+
+      <div style={{ marginTop: 20 }}>
+        <button onClick={() => setPage(p => Math.max(p - 1, 1))}>
+          Anterior
+        </button>
+
+        <span style={{ margin: "0 10px" }}>
+          Página {page}
+        </span>
+
+        <button onClick={() => setPage(p => p + 1)}>
+          Siguiente
+        </button>
+      </div>
     </div>
   );
 }
